0e3018009bf1ae4bffa8295a007291bd37588645
Jorge Larumbe <jorge.larumbem@gmail.com>
2024-02-13 10:19:32 -0500

    added additional comments explaining how the end points work

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -3,14 +3,22 @@ import express from "express";
 // Create a new express router
 const weatherRoute = express.Router();
 
-// GET request using a city to localhost:4000/weather/:city
+// GET request using a city to localhost:4000/weather/[city]
 weatherRoute.get("/:city", (req, res) => {
-  res.send(`The weather in ${req.params.city} is 25 degrees`);
+  // Get the city from the request parameters
+  const { city } = req.params;
+
+  // Send a response with the weather in the city
+  res.send(`The weather in ${city} is 25 degrees`);
 });
 
-// GET request using longitude and latitude to localhost:4000/weather/:lon/:lan
+// GET request using longitude and latitude to localhost:4000/weather/[lon]/[lan]
 weatherRoute.get("/:lon/:lan", (req, res) => {
-  res.send(`The weather at ${req.params.lon}, ${req.params.lan} is 25 degrees`);
+  // Get the longitude and latitude from the request parameters
+  const { lon, lan } = req.params;
+
+  // Send a response with the weather at the longitude and latitude
+  res.send(`The weather at ${lon}, ${lan} is 25 degrees`);
 });
 
 export default weatherRoute;
