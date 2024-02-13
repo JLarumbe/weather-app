9387885f67b8cd7981726f918b53a0aefcaeb4b6
Jorge Larumbe <jorge.larumbem@gmail.com>
2024-02-12 20:18:59 -0500

    Setup basic express server on port 4000 and setup up environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an express app
 const app = express();
 
+// Middleware to log all requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Listen on port 4000
 app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
