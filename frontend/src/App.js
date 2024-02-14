d0ac13e9aa42698b591e827e15130a65904df48b
Jorge Larumbe <jorge.larumbem@gmail.com>
2024-02-14 09:36:08 -0500

    created a compenents folder and the WeatherCard component that will display the weather info on a bootstrap made card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherCard from "./components/WeatherCard.js";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="jumbotron text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+        <WeatherCard />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/WeatherCard.js
@@ -0,0 +1,9 @@
+const WeatherCard = ({ weather }) => {
+  return (
+    <>
+      <h1>WeatherCard</h1>
+    </>
+  );
+};
+
+export default WeatherCard;
